91cf95891ee7445cc4e7125d64202f605aa24574
reidar-qlok <reidar-nilsen@qlok.se>
2024-02-09 22:09:18 +0100

    Redo att publicera

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./webpages/Home";
+import About from "./webpages/About";
+import Pictures from "./webpages/Pictures";
+import Menu from "./webpages/Menu";
+import logo from "./logo.svg";
+import Footer from "./Footer";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="site-container">
+        {" "}
+        {/* Flex container */}
+        <Menu />
+        <div className="content-wrap">
+          {" "}
+          {/ * Innehåll, sträcks för att fylla utrymmet */}
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/pictures" element={<Pictures />} />
+          </Routes>
+        </div>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
